be948280f527b8cf2bf217ad8690deaebccc5ebd
Ansgar Konermann <ansgar.konermann@googlemail.com>
2012-01-08 21:50:18 +0100

    [JODRP-0] POM and License text tweaking for release (release does reformat on XML-Tag-like structures)

--- src/main/java/org/jodreports/test/assertions/CellAssert.java
+++ src/main/java/org/jodreports/test/assertions/CellAssert.java
@@ -15,7 +15,7 @@
  * See the GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
+ * with this program. If not, see http://www.gnu.org/licenses/
  */
 package org.jodreports.test.assertions;
 
